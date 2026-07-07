88054e0b09c88c827cd1011fd49dce439d059ac0
lanebo1 <kirillefimovic141@gmail.com>
2026-07-07 14:06:10 +0300

    fix: update frontend build, fixed db migrations errors

--- admin/src/features/bookings/index.ts
+++
@@ -1,4 +0,0 @@
-export { BookingCreate } from "./BookingCreate";
-export { BookingList } from "./BookingList";
-export { BookingShow } from "./BookingShow";
-export { BookingStatusEdit } from "./BookingStatusEdit";
